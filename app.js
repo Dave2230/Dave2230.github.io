41c360d7a83619962c77e8deaa3a3b706800e8f8
Dave2230 <dsymons2230@gmail.com>
2021-03-11 17:06:15 +0000

    20210311

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let sliderTrigger = document.getElementsByClassName("slider-trigger")[0];
 let slider = document.getElementsByClassName('slider-parent')[0];
 
-sliderTrigger.addEventListener("click", function (el) {
+sliderTrigger.addEventListener("click", function () {
     if (slider.classList.contains("active")) {
         slider.classList.remove("active");
     } else { slider.classList.add("active"); }
 });
+
+console.log(getComputedStyle(sliderTrigger).display);
--- wordCycle.js
+++ wordCycle.js
@@ -240,12 +240,3 @@ const wordCycleInterval = setInterval(function () {
 }
     , duration
 )
-
-let sliderTrigger = document.getElementsByClassName("slider-trigger")[0];
-let slider = document.getElementsByClassName('slider-parent')[0];
-
-sliderTrigger.addEventListener("click", function (el) {
-    if (slider.classList.contains("active")) {
-        slider.classList.remove("active");
-    } else { slider.classList.add("active"); }
-});
